c03c08f76a805caf65f5f9e1738fe3d57df40051
abhiram47 <abhiram5871@gmail.com>
2025-11-10 15:34:33 +0530

    Implemented game state machine and stats tracking

--- main.c
+++ main.c
@@ -25,22 +25,126 @@ double getTimeInMS() {
 #endif
 }
 
+typedef enum {
+    STATE_MENU,
+    STATE_READY,
+    STATE_WAITING,
+    STATE_FIRED,
+    STATE_RESULT,
+    STATE_EXIT
+} GameState;
+
+typedef struct {
+    double best_time;
+    double worst_time;
+    double average_time;
+    double total_time;
+    int games_played;
+}PlayerStats;
+
+typedef struct {
+    int delay_min;
+    int delay_max;
+    PlayerStats stats;
+    GameState current_state;
+} GameSettings;
+
 int main() {
     srand(time(NULL));   
-    int delay = rand()%7001 + 3000; // Random delay between 3000ms to 13000ms
-
-    printf("Get ready...\n");
-    Sleep(delay);
-    printf("[FIRE!]\n");
-
-    // Using Windows high-resolution timer
-    double startTime = getTimeInMS();
-    getch();
-    double endTime = getTimeInMS();
+    GameSettings Settings;
+    Settings.delay_min = 3000;
+    Settings.delay_max = 7000;
+    Settings.stats.best_time = 9999.0;
+    Settings.stats.worst_time = 0.0;
+    Settings.stats.average_time = 0.0;
+    Settings.stats.total_time = 0.0;
+    Settings.stats.games_played = 0;
+    Settings.current_state = STATE_MENU;
     
-    double reaction_time = endTime - startTime; // Already in milliseconds
-    printf("Your reaction time is: %.2f ms\n", reaction_time);
-    
+    while (Settings.current_state != STATE_EXIT) {
+        // Game continues until exit
+        switch (Settings.current_state) {
+
+            case STATE_MENU:
+                printf("You are in the menu.\n");
+                printf("Select an option to proceed...\n\n");
+                printf("options...\n");
+                printf("\n");
+                printf("option 1 -> Start Game\n");
+                printf("option 2 -> View Stats\n");
+                printf("option 3 -> Quit\n");
+                
+                char choice = getch();
+                if (choice == '1') {
+                    Settings.current_state = STATE_READY;
+                } else if (choice == '2') {
+                    Settings.current_state = STATE_RESULT;
+                } else if (choice == '3') {
+                    Settings.current_state = STATE_EXIT;
+                } else {
+                    printf("Invalid option! Returning to menu...\n");
+                    Settings.current_state = STATE_MENU;
+                }
+                break;
+
+            case STATE_READY:
+                printf("Get ready...\n");
+                Settings.current_state = STATE_WAITING;
+                break;
+
+            case STATE_WAITING:
+                int random_delay = rand() % (Settings.delay_max - Settings.delay_min + 1) + Settings.delay_min;
+                printf("Waiting for random delay of %d ms...\n", random_delay);
+                Sleep(random_delay);
+                Settings.current_state = STATE_FIRED;
+                printf("\n");
+                break;
+
+            case STATE_FIRED:
+                printf("[FIRE!]\n");
+                printf("Press any key to stop the timer...\n");
+                double startTime = getTimeInMS();
+                getch();
+                double endTime = getTimeInMS();
+                double reactionTime = endTime - startTime;
+                printf("Your reaction time is: %.2f ms\n", reactionTime);
+                Settings.stats.games_played++;
+                Settings.stats.total_time += (reactionTime);
+                Settings.stats.average_time = Settings.stats.total_time / Settings.stats.games_played;
+                if (reactionTime < Settings.stats.best_time) {
+                    Settings.stats.best_time = reactionTime;
+                }
+                if (reactionTime > Settings.stats.worst_time) {
+                    Settings.stats.worst_time = reactionTime;
+                }
+                Settings.current_state = STATE_RESULT;
+                printf("\n");
+
+                break;
+            case STATE_RESULT:
+                printf("Displaying stats...\n");
+                printf("Best Time: %.2f ms\n", Settings.stats.best_time);
+                printf("Worst Time: %.2f ms\n", Settings.stats.worst_time);
+                printf("Average Time: %.2f ms\n", Settings.stats.average_time);
+                printf("Total Games Played: %d\n", Settings.stats.games_played);
+                printf("Returning to menu...\n");
+                printf("...\n");
+                printf("press any key to continue...\n");
+                getch();
+                Settings.current_state = STATE_MENU;   
+                printf("\n");
+                break;  
+            
+            default:
+                printf("Unknown state!\n");
+                printf("Returning to menu...\n");
+                Settings.current_state = STATE_MENU;
+                break;
+        }
+    }
+
+    printf("Exiting the game. Goodbye!\n");
+    printf("\n");
 
     return 0;
 }
